31940b6da9f432900ac7d973753a1f219bedb563
Tristan Mangeard <tmangeard@gmx.fr>
2019-09-18 11:57:27 +1100

    add coverage test

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ from test_ci import addition
 
 def test_addition():
     assert addition(1, 1) == 2
+    assert addition(-1, -1) == -2
+    assert addition(-1, 1) == 0
+    assert addition(1, -1) == 0
